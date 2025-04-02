5c132252998ef87809da304dc5b3c71a056b0541
Suryasgithub1 <dsurya27299@gmail.com>
2025-04-02 23:44:57 +0530

    Changed Search Results for Dynamic Name

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+
 
 const SearchResults = () => {
+  const [searchParams] = useSearchParams();
+
+  const searchQuery = searchParams.get('search_query');
+  // console.log(searchQuery)
+
   return (
     <div>
-      <p>Hi</p>
+      <p className='text-2xl text-gray-400'>{searchQuery}</p>
     </div>
   )
 }
