8472fd43dc50e7cd1daea634188f70a3e17284ba
Suryasgithub1 <dsurya27299@gmail.com>
2025-04-09 22:24:38 +0530

    Fixed the time and views VideoCard numbers

--- src/components/ButtonsBuild.jsx
+++ src/components/ButtonsBuild.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const ButtonsBuild = (info) => {
+  let buttonClass = "w-auto bg-gray-100 mx-3 p-2 rounded-[10px] cursor-pointer";
+  const location = useLocation();
+  const url = new URL(window.location.href);
+  const searchParams = new URLSearchParams(url.search);
+  const query = searchParams.get('search_query');
+  const currentLocation = (location.pathname)
+  //console.log(currentLocation)
+  console.log(query)
+
   const navigate = useNavigate();
   
   const handleClick = (e) => {
     navigate(`/results?search_query=${e.target.textContent.replace(/ /g, '')}`);
+    if (currentLocation === '/'){
+      buttonClass = "w-auto bg-gray-600 mx-3 p-2 rounded-[10px] cursor-pointer";
+    } else if ((info.info)== query ){
+      buttonClass = "w-auto bg-gray-200 mx-3 p-2 rounded-[10px] cursor-pointer";
+  }
   };
 
-  let buttonClass = "w-auto bg-gray-200 mx-3 p-2 rounded-[10px] cursor-pointer";
-
-  if (info.info === "All") {
-    buttonClass = "w-auto bg-gray-800 mx-3 p-2 rounded-[10px] cursor-pointer text-white";
-  }
 
   return (
     <button className= {` ${buttonClass}`} 
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -11,7 +11,10 @@ const VideoCard = ({items}) => {
           src={items?.snippet?.thumbnails?.medium?.url}
           alt={items?.snippet?.title}
         />
-        <h1 className="font-['Roboto','Arial',sans-serif] text-[16px] font-[900] w-[296.320px] mb-[5px] h-[46px] overflow-hidden">{items?.snippet?.title}</h1>
+        <h1 className="font-['Roboto','Arial',sans-serif] text-[16px] font-[900] w-[296.320px] mb-[5px] h-[46px] overflow-x: hidden;
+">
+            {items?.snippet?.title?.length > 65 ?( (items?.snippet?.title).substring(0, 61) + "..." ): (items?.snippet?.title) }
+              </h1>
         <h2 className="text-[14px] text-gray-400  font-['Roboto','Arial',sans-serif]" >{items?.snippet?.channelTitle}</h2>
         <div className="flex">
           <h3 className="text-[14px] text-gray-400 font-['Roboto','Arial',sans-serif]">{viewsCount(items?.statistics?.viewCount)}</h3>
